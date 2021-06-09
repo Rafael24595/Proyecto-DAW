550442c55efde2c5c65b7f8e4e184139e3eabe9f
Rafael Ruiz <rafael.ruiz@sisnet360.com>
2021-06-09 09:27:40 +0200

    [FIX] Cambio puerto

--- Myvinyl-Angular/src/utils/variables/variables.ts
+++ Myvinyl-Angular/src/utils/variables/variables.ts
@@ -20,7 +20,7 @@ export const SearchQuery:SearchQueryInterface = {
 export const Variables = {
         candyIdWhiteList : ['candy-home'],
         candyDeadEnd:['theme'],
-        host:'3.15.187.168',
+        host:'3.143.232.30',
         port:'2525',
         range:{0:'User', 1:'Admin'}
 }
